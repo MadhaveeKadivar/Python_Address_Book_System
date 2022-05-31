e3f8a2a7acfc197fb7845a68df914dbdd7963503
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-31 18:52:44 +0530

    [Madhavee] Add : Multiple records in address book

--- AddressBookMainClass.py
+++ AddressBookMainClass.py
@@ -2,7 +2,7 @@
     @Author: Madhavee Kadivar
     @Date: 2022-05-30 19:13:07
     @Last Modified by: Madhavee Kadivar
-    @Last Modified time: 2022-05-31 09:01:37
+    @Last Modified time: 2022-05-31 02:22:49
     @Title : Address Book System
 '''
 # Import 
@@ -13,15 +13,18 @@ records = Addressbook()
 print("\n1.Add a new Record\n2.Update Records\nDelete Records")
 ch = int(input("\nEnter your choice : "))
 if ch == 1:
-    fname = input("\nEnter your First Name : ")
-    lname = input("Enter your Last Name : ")
-    address = input("Enter your Address : ")
-    city = input("Enter your City Name : ")
-    state = input("Enter your State Name : ")
-    zip = input("Enter your Zip Code : ")
-    phone_number = input("Enter your Phone Number : ")
-    email = input("Enter your Email : ")
-    records.add_records(fname,lname,address,city,state,zip,phone_number,email) # Calling a method of AddressBook class to add record in address book
+    num = int(input("How many contacts you want to add in address book : "))
+    for i in range(num):
+        print(f"\nEnter details for contact {i+1} : \n ")
+        fname = input("\nEnter your First Name : ")
+        lname = input("Enter your Last Name : ")
+        address = input("Enter your Address : ")
+        city = input("Enter your City Name : ")
+        state = input("Enter your State Name : ")
+        zip = input("Enter your Zip Code : ")
+        phone_number = input("Enter your Phone Number : ")
+        email = input("Enter your Email : ")
+        records.add_records(fname,lname,address,city,state,zip,phone_number,email) # Calling a method of AddressBook class to add record in address book
     records.print_records() # Calling a method of AddressBook class to display records of address book
 elif ch == 2:
     old_fname = input("\nEnter your First Name : ")
--- Testing.py
+++ Testing.py
@@ -23,68 +23,74 @@ class TestArithmeticOperation(unittest.TestCase):
     #     obj = CreateContacts()
     #     self.assertEquals(obj.person_input() ,(obj.fname,obj.lname,obj.address,obj.city,obj.state,obj.zip,obj.phone_number,obj.email ))
 
-    def test_add_records(self):
-        """ 
-        Description: 
-            This function is testing add records method of Addressbook Class
-        Parameter:
-            It takes self as argument
-        Return:
-            returns Nothing
-        """
-        #per = CreateContacts('abc','xyz','pqr','kmn','uvw','123','1234567890','mk@gmail.com')
-        ab = self.person.add_records('abc','xyz','pqr','kmn','uvw','123','1234567890','mk@gmail.com')
-        self.assertEqual(ab[0].fname, 'abc')
-        self.assertEqual(ab[0].lname, 'xyz')
-        self.assertEqual(ab[0].address, 'pqr')
-        self.assertEqual(ab[0].city, 'kmn')
-        self.assertEqual(ab[0].state, 'uvw')
-        self.assertEqual(ab[0].zip, '123')
-        self.assertEqual(ab[0].phone_number, '1234567890')
-        self.assertEqual(ab[0].email, 'mk@gmail.com')
-        #self.assertListEqual(list(ab[0]),per)
+    # def test_add_records(self):
+    #     """ 
+    #     Description: 
+    #         This function is testing add records method of Addressbook Class
+    #     Parameter:
+    #         It takes self as argument
+    #     Return:
+    #         returns Nothing
+    #     """
+    #     #per = CreateContacts('abc','xyz','pqr','kmn','uvw','123','1234567890','mk@gmail.com')
+    #     ab = self.person.add_records('abc','xyz','pqr','kmn','uvw','123','1234567890','mk@gmail.com')
+    #     self.assertEqual(len(ab),1)
+    #     #self.assertEqual(ab[0]),per)
 
-    def test_find_records(self):
-        """ 
-        Description: 
-            This function is testing find records method of Addressbook Class
-        Parameter:
-            It takes self as argument
-        Return:
-            returns Nothing
-        """
-        self.assertTrue(self.person.find_records('abc'))
+    # def test_find_records(self):
+    #     """ 
+    #     Description: 
+    #         This function is testing find records method of Addressbook Class
+    #     Parameter:
+    #         It takes self as argument
+    #     Return:
+    #         returns Nothing
+    #     """
+    #     self.assertTrue(self.person.find_records('abc'))
         
-    def test_update_records(self):
-        """ 
-        Description: 
-            This function is testing update records method of Addressbook Class
-        Parameter:
-            It takes self as argument
-        Return:
-            returns Nothing
-        """
-        ab = self.person.update_records('abc','abc','Kadivar','pqr','kmn','uvw','123','1234567890','mk@gmail.com')
-        self.assertEqual(ab[0].fname, 'abc')
-        self.assertEqual(ab[0].lname, 'Kadivar')
-        self.assertEqual(ab[0].address, 'pqr')
-        self.assertEqual(ab[0].city, 'kmn')
-        self.assertEqual(ab[0].state, 'uvw')
-        self.assertEqual(ab[0].zip, '123')
-        self.assertEqual(ab[0].phone_number, '1234567890')
-        self.assertEqual(ab[0].email, 'mk@gmail.com')
+    # def test_update_records(self):
+    #     """ 
+    #     Description: 
+    #         This function is testing update records method of Addressbook Class
+    #     Parameter:
+    #         It takes self as argument
+    #     Return:
+    #         returns Nothing
+    #     """
+    #     ab = self.person.update_records('abc','abc','Kadivar','pqr','kmn','uvw','123','1234567890','mk@gmail.com')
+    #     self.assertEqual(ab[0].fname, 'abc')
+    #     self.assertEqual(ab[0].lname, 'Kadivar')
+    #     self.assertEqual(ab[0].address, 'pqr')
+    #     self.assertEqual(ab[0].city, 'kmn')
+    #     self.assertEqual(ab[0].state, 'uvw')
+    #     self.assertEqual(ab[0].zip, '123')
+    #     self.assertEqual(ab[0].phone_number, '1234567890')
+    #     self.assertEqual(ab[0].email, 'mk@gmail.com')
 
-    def test_delete_records(self):
+    # def test_delete_records(self):
+    #     """ 
+    #     Description: 
+    #         This function is testing delete records method of Addressbook Class
+    #     Parameter:
+    #         It takes self as argument
+    #     Return:
+    #         returns Nothing
+    #     """ 
+    #     ab = self.person.delete_record('abc')
+    #     self.assertEqual(len(ab),0)
+
+    def test_add_multiple_records(self):
         """ 
         Description: 
-            This function is testing delete records method of Addressbook Class
+            This function is testing add records method of Addressbook Class to add multiple records
         Parameter:
             It takes self as argument
         Return:
             returns Nothing
         """ 
-        ab = self.person.delete_record('abc')
-        self.assertEqual(len(ab),0)
+        self.person.add_records('abc','xyz','pqr','kmn','uvw','123','1234567890','mk@gmail.com')
+        ab = self.person.add_records('Madhavee','Kadivar','Jamjodhpur','Jamnagar','Gujarat','123','1234567890','mk@gmail.com')
+        self.assertEqual(len(ab),2)
         
 if __name__ == "__main__":
     unittest.main()
